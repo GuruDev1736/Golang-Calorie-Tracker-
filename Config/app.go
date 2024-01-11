a3fcc8c7c9fa7478d85e51b90c9f2bf38d7d793f
Guruprasad Bhagat <guruprasad1736@gmail.com>
2024-01-11 21:08:06 +0530

    Done

--- Config/app.go
+++ Config/app.go
@@ -1,43 +1,27 @@
 package config
 
 import (
-	"fmt"
-	"os"
-
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	"github.com/joho/godotenv"
 )
 
 var (
 	db *gorm.DB
 )
 
-func Connect() error {
-	if err := godotenv.Load(); err != nil {
-		return fmt.Errorf("Error loading .env file: %s", err)
-	}
-
-	// Connect to the database using environment variables
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"))
-
-	d, err := gorm.Open("mysql", dsn)
+func Connect() {
+	// mysql://root:EEf334-BHbDBDCgCFh1d3adg6cb4b561@monorail.proxy.rlwy.net:16383/railway
+	d, err := gorm.Open("mysql", "root:EEf334-BHbDBDCgCFh1d3adg6cb4b561@monorail.proxy.rlwy.net:16383/railway")
 	if err != nil {
-		return fmt.Errorf("Error connecting to the database: %s", err)
+		panic(err)
 	}
-
-	// Set up a database connection pool
-	d.DB().SetMaxOpenConns(10) // Adjust as needed
-	d.DB().SetMaxIdleConns(5)  // Adjust as needed
 
 	db = d
-	return nil
+
 }
 
 func GetDB() *gorm.DB {
+
 	return db
+
 }
